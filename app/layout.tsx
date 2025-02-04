0bb4acd79f58a3a7172a836ecf38c0245819989a
monangbahana <monangbahana@gmail.com>
2025-02-04 15:34:01 +0000

    first-commit

---
+++ app/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import localFont from "next/font/local";
+import { ReactNode } from "react";
+import { Toaster } from "@/components/ui/toaster"
+
+const ibmPlexSans = localFont({
+  src: [
+    { path: "/fonts/IBMPlexSans-Regular.ttf", weight: "400", style: "normal" },
+    { path: "/fonts/IBMPlexSans-Medium.ttf", weight: "500", style: "normal" },
+    { path: "/fonts/IBMPlexSans-SemiBold.ttf", weight: "600", style: "normal" },
+    { path: "/fonts/IBMPlexSans-Bold.ttf", weight: "700", style: "normal" },
+  ],
+});
+
+const bebasNeue = localFont({
+  src:[
+    { path: "/fonts/BebasNeue-Regular.ttf", weight: "400", style: "normal" },
+  ],
+  variable: '--bebas-neue',
+});
+
+export const metadata: Metadata = {
+  title: "BukaBuku",
+  description: "Sistem Informasi Manajemen Peminjaman Buku",
+};
+
+const RootLayout = ({children}: {children: ReactNode}) => {
+  return (
+    <html lang="en">
+      <body
+        className={`${ibmPlexSans.className} ${bebasNeue.variable} antialiased`}
+      >
+        {children}
+        <Toaster />
+      </body>
+    </html>
+  );
+}
+
+export default RootLayout;
+
